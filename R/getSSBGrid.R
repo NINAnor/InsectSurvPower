8a21a7a612984d89a9e20022fddeae33c17170ee
Jens Åström <jens.astrom@nina.no>
2018-05-28 14:07:35 +0200

    added connect and getssb function

---
+++ R/getSSBGrid.R
@@ -0,0 +1,33 @@
+#' getSSBGrid
+#'
+#' Retrieves the SSB grids, together with fylke and kommune names (and ids) overlapping the center of each grid cell, as an sf object.
+#'
+#' @param scale Scale of SSB grid to retrieve. Options are 10km, 5km, 1km and 500m.
+#' Note that smaller scales are time consuming!
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' fylke10km <- getSSBGrid()
+#' plot(fylke10km["fylke"])
+#' }
+#' @import sf
+
+getSSBGrid <- function(conn = con, scale = c("10km","5km", "1km", "500m")){
+
+  scale <- match.arg(scale)
+  ssb <- paste0("ssb_data_utm33n.ssb_", scale)
+
+  query <- paste0("SELECT s.ssbid, s.xcoor, s.ycoor, s.geom, f.\"FYLKESNUMMER\", f.navn fylke, k.\"KOMMUNENUMMER\", k.navn kommune
+  FROM ", ssb," s,
+  \"AdministrativeUnits\".\"Norway_County_Fylke_polygons\" f,
+  \"AdministrativeUnits\".\"Norway_Municipality_Kommune_polygons\" k
+  WHERE ST_Intersects(ST_Centroid(s.geom), f.geom)
+  AND ST_Intersects(ST_Centroid(s.geom), k.geom)
+  --ORDER BY s.ssbid
+  --LIMIT 10")
+
+  out <- st_read_db(conn = conn, query = query, geom_column = "geom")
+  return(out)
+}
---
+++ R/postgresConnect.R
@@ -0,0 +1,29 @@
+#' postgresConnect
+#'
+#'
+#'
+#' @param user Username, defaults to guest account with basic read privileges
+#' @param password Password Beware of storing this in cleartext in scripts. Consider storing it in a file that is included in gitignore.
+#' @param host Name of database server. Defaults to (new) main PostgreSQL machine "ninpgsql03.nina.no".
+#'
+#' @import DBI
+#' @import RPostgres
+#' @export
+
+postgresConnect <- function(Username = "postgjest", Password = "gjestpost", host = "ninpgsql03.nina.no"){
+
+  if (!requireNamespace("DBI", quietly = TRUE)) {
+    stop("Pkg needed for this function to work. Please install it using devtools::install_github(\"rstats-db/DBI\") ",
+         call. = FALSE)
+  }
+
+
+  if (!requireNamespace("RPostgres", quietly = TRUE)) {
+    stop("Pkg needed for this function to work. Please install it using devtools::install_github(\"rstats-db/RPostgres\") ",
+         call. = FALSE)
+  }
+
+
+  tmp <- DBI::dbConnect(RPostgres::Postgres(), host = host, dbname = "gisdata", user = Username, password = Password)
+  assign("con", tmp, .GlobalEnv)
+}
